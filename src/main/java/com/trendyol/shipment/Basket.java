308c24a2150eba45276401b9bf436310758fc1e1
ahmetfatihay <77299600+ahmetfatihay@users.noreply.github.com>
2023-09-09 22:43:50 +0300

    test OK

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,52 @@
 package com.trendyol.shipment;
 
-import java.util.List;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class Basket {
+
+    final static Logger LOGGER = Logger.getLogger(Basket.class.getName());
 
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
+        try {
+            if (products.stream().anyMatch(product -> product.getSize().equals(ShipmentSize.X_LARGE))) {
+                return ShipmentSize.X_LARGE;
+            }
+            ShipmentSize result = isThereSizeMoreThanTwo();
+            if(Objects.nonNull(result)){
+                return result;
+            }
+            return biggestSize();
+        } catch (UnknownError ex){
+            LOGGER.log( Level.SEVERE, "ERROR=", ex );
+            return null;
+        }
+    }
+
+    public ShipmentSize biggestSize() {
+        List<ShipmentSize> sizeList = Arrays.asList(ShipmentSize.values());
+        Collections.reverse(sizeList);
+        for(ShipmentSize size : sizeList ) {
+            if(products.stream().anyMatch(product -> product.getSize().equals(size))) {
+                return size;
+            }
+        }
         return null;
+    }
+
+    public ShipmentSize isThereSizeMoreThanTwo(){
+        Map<ShipmentSize, Long> sizeCountMap = products.stream()
+                .collect(Collectors.groupingBy(Product::getSize, Collectors.counting()));
+
+        ShipmentSize moreThanThree = sizeCountMap.entrySet().stream()
+                .filter(entry -> entry.getValue() >= 3)
+                .map(Map.Entry::getKey).findAny().orElse(null);
+
+        return (Objects.nonNull(moreThanThree) ? moreThanThree.getNextSize() : moreThanThree);
     }
 
     public List<Product> getProducts() {
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -5,5 +5,17 @@ public enum ShipmentSize {
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+
+    public ShipmentSize getNextSize() {
+        ShipmentSize[] sizes = ShipmentSize.values();
+        int currentIndex = this.ordinal();
+
+        if (currentIndex < sizes.length - 1) {
+            return sizes[currentIndex + 1];
+        } else {
+            return sizes[currentIndex];
+        }
+    }
+
 }
