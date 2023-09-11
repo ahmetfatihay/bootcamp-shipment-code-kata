ec4f6553ad789c08abbd8c14906ba3965d0cab7f
ahmetfatihay <77299600+ahmetfatihay@users.noreply.github.com>
2023-09-11 23:10:09 +0300

    clear code

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,52 +1,51 @@
 package com.trendyol.shipment;
 
-import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
+import java.util.Objects;
+import java.util.Map;
+import java.util.Collections;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Basket {
 
-    final static Logger LOGGER = Logger.getLogger(Basket.class.getName());
-
     private List<Product> products;
 
     public ShipmentSize getShipmentSize() {
-        try {
-            if (products.stream().anyMatch(product -> product.getSize().equals(ShipmentSize.X_LARGE))) {
-                return ShipmentSize.X_LARGE;
-            }
-            ShipmentSize result = isThereSizeMoreThanTwo();
-            if(Objects.nonNull(result)){
-                return result;
-            }
-            return biggestSize();
-        } catch (UnknownError ex){
-            LOGGER.log( Level.SEVERE, "ERROR=", ex );
-            return null;
+        if (isThereXlarge()) {
+            return ShipmentSize.X_LARGE;
         }
+        ShipmentSize result = isThereSizeMoreThanTwo();
+        if (Objects.nonNull(result)) {
+            return result;
+        }
+        return biggestSize();
     }
 
-    public ShipmentSize biggestSize() {
-        List<ShipmentSize> sizeList = Arrays.asList(ShipmentSize.values());
-        Collections.reverse(sizeList);
-        for(ShipmentSize size : sizeList ) {
-            if(products.stream().anyMatch(product -> product.getSize().equals(size))) {
-                return size;
-            }
-        }
-        return null;
+    private boolean isThereXlarge() {
+        return products.stream().anyMatch(product -> product.getSize().equals(ShipmentSize.X_LARGE));
     }
 
-    public ShipmentSize isThereSizeMoreThanTwo(){
+    private ShipmentSize isThereSizeMoreThanTwo() {
         Map<ShipmentSize, Long> sizeCountMap = products.stream()
                 .collect(Collectors.groupingBy(Product::getSize, Collectors.counting()));
 
         ShipmentSize moreThanThree = sizeCountMap.entrySet().stream()
                 .filter(entry -> entry.getValue() >= 3)
-                .map(Map.Entry::getKey).findAny().orElse(null);
+                .map(Map.Entry::getKey).findFirst().orElse(null);
 
-        return (Objects.nonNull(moreThanThree) ? moreThanThree.getNextSize() : moreThanThree);
+        return (Objects.nonNull(moreThanThree) ? moreThanThree.getNextSize() : null);
+    }
+
+    private ShipmentSize biggestSize() {
+        List<ShipmentSize> shipmentSizeEnumList = Arrays.asList(ShipmentSize.values());
+        Collections.reverse(shipmentSizeEnumList);
+        for (ShipmentSize size : shipmentSizeEnumList) {
+            if (products.stream().anyMatch(product -> product.getSize().equals(size))) {
+                return size;
+            }
+        }
+        return null;
     }
 
     public List<Product> getProducts() {
